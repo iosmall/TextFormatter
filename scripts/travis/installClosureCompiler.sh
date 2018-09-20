7975f7c87df6bf5ce5ff7cb2635e7723901b5152
JoshyPHP <s9e.dev@gmail.com>
2018-09-20 22:35:03 +0200

    WiP

--- scripts/travis/installClosureCompiler.sh
+++ scripts/travis/installClosureCompiler.sh
@@ -3,5 +3,5 @@
 wget -O - http://dl.google.com/closure-compiler/compiler-latest.tar.gz | tar xzf - -C/tmp && \
 mv /tmp/closure-compiler-*.jar /tmp/compiler.jar
 
-cd "$(dirname $(dirname $(dirname $0)))"/vendor
+cd "$(dirname $0)"/../../vendor
 npm i google-closure-compiler-linux
