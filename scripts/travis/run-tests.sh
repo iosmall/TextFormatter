0bc99ac1cbe2b0108c5c592a2410535a8ca8335c
JoshyPHP <s9e.dev@gmail.com>
2019-02-19 02:12:26 +0100

    WiP

--- scripts/travis/run-tests.sh
+++ scripts/travis/run-tests.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
 
-cd $(dirname "$0")
-cd ../..
-
-CMD=phpunit
-if [ -f ./vendor/bin/phpunit ]
-then
-	CMD=./vendor/bin/phpunit
-fi
-
-if [ -n "$COVERAGE" ]
-then
-	# Run the network tests in parallel to populate the cache
-	CACHE_PRELOAD=1 $CMD --group needs-network tests/Plugins/MediaEmbed/ParserTest.php > /dev/null &
-
-	$CMD --exclude-group needs-js --coverage-clover /tmp/clover.xml
-else
-	$CMD --exclude-group needs-network
-fi
+ls -l /usr/bin
